82986d8558f3d8a4c4a30ba4cf7ca92215122490
Steven King <kingsteven1113@gmail.com>
2024-03-04 19:23:40 -0500

    starting project

---
+++ monument-selection-guide/src/Components/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='Welcome'>
+        <h1>Wlecome to the Monument Selection Guide</h1>
+        
+    </div>
+  )
+}
+
+export default LandingPage
