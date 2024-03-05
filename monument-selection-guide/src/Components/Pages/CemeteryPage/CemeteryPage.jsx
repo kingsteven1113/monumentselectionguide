adf1b86116937eab9f949bd0b9f87473ed1a266e
kingsteven1113 <125849798+kingsteven1113@users.noreply.github.com>
2024-03-05 10:44:01 -0500

    Update CemeteryPage.jsx

--- monument-selection-guide/src/Components/Pages/CemeteryPage/CemeteryPage.jsx
+++ monument-selection-guide/src/Components/Pages/CemeteryPage/CemeteryPage.jsx
@@ -1,1 +1,27 @@
+import React from 'react'
+import './CemeteryPage.css'
 
+const CemeteryPage = () => {
+  const OpenNextPage = () => {
+    const Cemetery = document.getElementById("CemeteryPage")
+      Cemetery.style.display = "none"
+    const Budget = document.getElementById("Budget")
+      Budget.style.display = "none"
+  }
+  return (
+    <div className='Header'>
+        <h1>What <span>cemetery</span> is this stone going to?</h1>
+       <div className='Line'></div>
+      <div>
+        <ul>
+
+          // Need to make a Cemetery API and call it so that we don't need to write them all in individually.
+          
+        </ul>
+      </div>
+       
+    </div>
+  )
+}
+
+export default LandingPage
