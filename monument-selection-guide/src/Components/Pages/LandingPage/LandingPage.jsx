64cadf1783771ab10ccc1ebe18f159398281b8d2
Steven King <kingsteven1113@gmail.com>
2024-03-04 19:27:35 -0500

    organization

--- monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
+++ monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
   return (
     <div className='Welcome'>
         <h1>Wlecome to the Monument Selection Guide</h1>
-        
+       <div className='Line'></div> 
     </div>
   )
 }
