2c07e4d95d2cc8ddcf0d47a60a61c806e740152c
Steven King <kingsteven1113@gmail.com>
2024-03-04 22:47:57 -0500

    got started

--- monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
+++ monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
@@ -6,9 +6,9 @@ const LandingPage = () => {
     <div className='Welcome'>
         <h1>Welcome to the Monument Selection Guide</h1>
        <div className='Line'></div>
-       <p>This is a step by step guide that will lead you to making the best monument for your loved one within your budget.</p>
+       <p className='Description'>This is a step by step guide that will lead you to making the best monument for your loved one within your budget.</p>
        <button className='GetStarted'> Get Started</button>
-       <p>Please note thta this will <span>not</span> keep progress if you close the browser. You will have to start over if you leave.</p>
+       <p className='Notice'>Please note that this will <span>not</span> keep progress if you close the browser. You will have to start over if you leave.</p>
     </div>
   )
 }
