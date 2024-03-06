b5f0a3dd2043bf4587c2f9d6043d617c83719b75
kingsteven1113 <125849798+kingsteven1113@users.noreply.github.com>
2024-03-06 08:30:39 -0500

    Update EmailFormSubmission.js

--- monument-selection-guide/src/assets/external-scripts/EmailFormSubmission.js
+++ monument-selection-guide/src/assets/external-scripts/EmailFormSubmission.js
@@ -1,1 +1,3 @@
-const FormSubmissionActionLink = 
+const FormSubmissionActionLink = "https://script.google.com/macros/s/AKfycbyFV7d1t2maciSgHfGuYG5EHPnnjuXEvinNnRVHLTfo4MWiDz9AEoSik_FlsaLhAojK1Q/exec"
+
+export default FormSubmissionActionLink
