9c225faa5fbc1008d55fef6940aa19197c7f7127
Steven King <kingsteven1113@gmail.com>
2024-03-07 00:34:30 -0500

    cemetery

--- monument-selection-guide/src/App.jsx
+++ monument-selection-guide/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './Components/Pages/LandingPage/LandingPage'
+import CemeteryPage from './Components/Pages/CemeteryPage/CemeteryPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
 
   return (
     <> 
-      <LandingPage id="Welcome"></LandingPage>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}></Route>
+        <Route path='/cemetery' element={<CemeteryPage/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
+++ monument-selection-guide/src/Components/Pages/LandingPage/LandingPage.jsx
@@ -1,37 +1,19 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import './LandingPage.css'
-import FormSubmissionActionLink from ".../assets/external-scripts/EmailFormSubmission"
+import FormSubmissionActionLink from "../../../assets/external-scripts/EmailFormSubmission"
+import CemeteryPage from '../CemeteryPage/CemeteryPage'
+
 
 const LandingPage = () => {
-  const OpenNextPage = () => {
-    const Cemetery = document.getElementById("CemeteryPage")
-      Cemetery.style.display = "block"
-    const WelcomePage = document.getElementById("Welcome")
-      WelcomePage.style.display = "none"
-  }
+ 
   return (
     <div className='Welcome'>
         <h1>Welcome to the Monument Selection Guide</h1>
        <div className='Line'></div>
        <p className='Description'>This is a step by step guide that will lead you to making the best monument for your loved one within your budget.</p>
-       <button className='GetStarted'> Get Started</button>
-       <p className='Notice'>*Please note that this will <span>not</span> keep progress if you close the browser. You will have to start over if you leave the browser.*</p>
-       <form method='POST' data-email="kingsteven1113@gmail.com" className='gform' action={FormSubmissionActionLink}>
-        <input name='Timestamp' type="text" />
-        <input name='First Name' type="text" />
-        <input name='Last Name' type="text" />
-        <input name='Email' type="text" />
-        <input name='Color' type="text" />
-        <input name='Cemetery' type="text" />
-        <input name='Budget' type="text" />
-        <input name='Shape' type="text" />
-        <input name='Finishes' type="text" />
-        <input name='Designs' type="text" />
-        <input name='Lettering' type="text" />
-        <input name='Wording' type="text" />
-        <input name='Type' type="text" />
-        <button type='submit'>Submit</button>
-       </form>
+       <NavLink to="/cemetery"><button className='GetStarted'> Get Started</button></NavLink>
+       
     </div>
   )
 }
