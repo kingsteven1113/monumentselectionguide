f420a0c55d68a2cbc93c164dca6b63b9133cf8da
kingsteven1113 <125849798+kingsteven1113@users.noreply.github.com>
2024-03-11 14:51:24 -0400

    Update SearchFilter.jsx

--- monument-selection-guide/src/Components/Pages/CemeteryPage/SearchFilter.jsx
+++ monument-selection-guide/src/Components/Pages/CemeteryPage/SearchFilter.jsx
@@ -69,27 +69,7 @@ const USERS = [
 
 export function App() {
   // the value of the search field 
-  const [name, setName] = useState('');
-
-  // the search result
-  const [foundUsers, setFoundUsers] = useState(USERS);
-
-  const filter = (e) => {
-    const keyword = e.target.value;
-
-    if (keyword !== '') {
-      const results = USERS.filter((user) => {
-        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
-        // Use the toLowerCase() method to make it case-insensitive
-      });
-      setFoundUsers(results);
-    } else {
-      setFoundUsers(USERS);
-      // If the text field is empty, show all users
-    }
-
-    setName(keyword);
-  };
+ 
 
   return (
     <div className="container">
